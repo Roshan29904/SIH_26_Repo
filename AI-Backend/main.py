c94eea91e20461181ff4350b7ccbd18265960bac
Utkarsh21116 <chaubeyutkarsh55@gmail.com>
2026-09-09 15:28:42 +0530

    Orchestration

--- AI-Backend/main.py
+++ AI-Backend/main.py
@@ -1,9 +1,8 @@
-from query_analysis.query_analyzer import analyze_query
-from model_routing.model_router import route_model
-from model_routing.model_config import MODEL_CONFIG
-from model_routing.model_manager import get_model
+from orchestration.orchestrator import build_graph
+
 
 def get_user_query():
+
     query = input("Enter your task: ")
 
     return {
@@ -18,30 +17,17 @@ if __name__ == "__main__":
     print("\nReceived Task:")
     print(task["query"])
 
-    print("\nAnalyzing task...")
+    graph = build_graph()
 
-    analysis = analyze_query(task["query"])
+    result = graph.invoke({
+        "query": task["query"]
+    })
 
-    print("\nQuery Analysis:")
-    print(analysis)
+    print("\nModel Role:")
+    print(result["model_role"])
 
-    # print("\nAs Dictionary:")
-    # print(analysis.model_dump())
-
-    selected_role = route_model(analysis)
-
-    selected_model = MODEL_CONFIG[selected_role]
-
-    print("\nModel Routing:")
-    print("Model Role:", selected_role)
-    print("Selected Model:", selected_model)
-
-    llm = get_model(selected_model)
-    print("\nSelected model loaded successfully.")
-
-    response = llm.invoke(
-    task["query"]
-    )
+    print("\nSelected Model:")
+    print(result["model_name"])
 
     print("\nModel Response:")
-    print(response.content)
+    print(result["response"])
---
+++ AI-Backend/orchestration/orchestrator.py
@@ -0,0 +1,66 @@
+from typing import TypedDict
+
+from query_analysis.query_analyzer import QueryAnalysis, analyze_query
+from model_routing.model_router import route_model
+from model_routing.model_config import MODEL_CONFIG
+from model_routing.model_manager import get_model
+
+from langgraph.graph import StateGraph
+
+
+class AgentState(TypedDict):
+
+    query: str
+    analysis: QueryAnalysis
+    model_role: str
+    model_name: str
+    response: str
+
+
+def analysis_node(state: AgentState):
+
+    analysis = analyze_query(state["query"])
+
+    return {
+        "analysis": analysis
+    }
+
+
+def routing_node(state: AgentState):
+
+    model_role = route_model(state["analysis"])
+    model_name = MODEL_CONFIG[model_role]
+
+    return {
+        "model_role": model_role,
+        "model_name": model_name
+    }
+
+
+def model_node(state: AgentState):
+
+    llm = get_model(state["model_name"])
+
+    response = llm.invoke(state["query"])
+
+    return {
+        "response": response.content
+    }
+
+
+def build_graph():
+
+    graph = StateGraph(AgentState)
+
+    graph.add_node("analyze", analysis_node)
+    graph.add_node("route", routing_node)
+    graph.add_node("generate", model_node)
+
+    graph.set_entry_point("analyze")
+
+    graph.add_edge("analyze", "route")
+    graph.add_edge("route", "generate")
+
+    graph.set_finish_point("generate")
+
+    return graph.compile()
